076da0c758117d93d817d8bbe5ed5211e6823722
Beirão <phsicopt@gmail.com>
2024-05-17 12:09:48 +0100

    Added personality

--- src/events/guildMemberAdd/welcome-message.js
+++ src/events/guildMemberAdd/welcome-message.js
@@ -12,7 +12,7 @@ module.exports = async (guildMember) => {
 		const welcomeChannel = guildMember.guild.channels.cache.get(welcomeChannelId);
 		if (!welcomeChannel) return;
 
-		const welcomeMessage = `Bem-vindo(a) ${guildMember.toString()}\nAcabaste de entrar no melhor grupo de patins de sempre\nPara teres uma ideia de como organizamos aqui o nosso discord vê o <#1220459599384018992>`;
+		const welcomeMessage = `Bem-vindo(a) ${guildMember.toString()} 🥳🥳🥳 \nAcabaste de entrar no melhor grupo de patins de sempre 🔥\nPara teres uma ideia de como organizamos aqui o nosso discord vê o <#1220459599384018992>`;
 
 		welcomeChannel.send({ content: welcomeMessage });
 	} catch (error) {
