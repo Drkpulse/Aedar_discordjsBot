dc7d1e0d36f1e2c490579f60052b16aa64424f9b
Beirão <phsicopt@gmail.com>
2024-05-17 11:59:21 +0100

    Changed welcome message

--- src/events/guildMemberAdd/welcome-message.js
+++ src/events/guildMemberAdd/welcome-message.js
@@ -12,7 +12,9 @@ module.exports = async (guildMember) => {
 		const welcomeChannel = guildMember.guild.channels.cache.get(welcomeChannelId);
 		if (!welcomeChannel) return;
 
-		const welcomeMessage = `Bem-vindo, ${guildMember.user.tag}! Verifica as regras e blah blah`;
+		const welcomeMessage = `Bem-vindo(a) ${guildMember.user.tag} \n
+		Acabaste de entrar no melhor grupo de patins de sempre \n
+		Para teres uma ideia de como organizamos aqui o nosso discord vê o <#1220459599384018992>`;
 
 		welcomeChannel.send({ content: welcomeMessage });
 	} catch (error) {
