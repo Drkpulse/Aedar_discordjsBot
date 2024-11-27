31d1908f4e2524af4ccb3b81140f7d945ae2d709
Beirão <zbeirao@proton.me>
2024-11-27 16:54:30 +0000

    [ADD] Random Yes or No

---
+++ src/commands/testing/yesorno.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('yesno')
+		.setDescription('Recebe uma resposta aleatória de "sim" ou "não"'),
+
+	run: async ({ interaction }) => {
+		try {
+			const response = await axios.get('https://yesno.wtf/api'); // Example API endpoint
+			const { answer, image } = response.data;
+
+			if (answer && image) {
+				await interaction.reply({
+					content: `**${answer}**`,
+					embeds: [{
+						image: {
+							url: image,
+						},
+					}],
+					ephemeral: false
+				});
+			} else {
+				throw new Error('No answer found.');
+			}
+		} catch (error) {
+			console.error('Error fetching yes/no response:', error);
+			await interaction.reply({ content: 'Ocorreu um erro ao buscar a resposta. Tente novamente mais tarde.', ephemeral: true });
+		}
+	},
+	options: {
+		cooldown: '10s',
+	},
+};
